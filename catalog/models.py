a2ec6113bf555cb4ee1b406fffe8a6abfee5619b
oleg-potichnyi <potichnyi.oleg@gmail.com>
2024-06-14 17:08:57 +0300

    Add models and migrations

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.username
+
+
+class Transaction(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="transactions"
+    )
+    transaction_type = models.CharField(max_length=50)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.transaction_type} - {self.amount}"
